2641bf9f5261924b64af186da3699ff96891395d
samwasted <samarpancodes@gmail.com>
2025-06-19 22:38:22 +0530

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
-import middleware from '@kinde-oss/kinde-auth-nextjs/middleware'
+import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware'
+export default withAuth({
+  // options here (e.g., loginPage, publicPaths, isAuthorized)
+});
 export const config = {
-    matcher: ["/dashboard/:path*", "/auth-callback"]
+  matcher: ["/dashboard/:path*", "/auth-callback"]
 }
-export default middleware
