3eb5fff7b27384e31305c3e20a4f59d61ae65304
samwasted <samarpancodes@gmail.com>
2025-06-20 16:42:16 +0530

    error fix

--- src/app/api/cancelSubscription/route.ts
+++ src/app/api/cancelSubscription/route.ts
@@ -4,7 +4,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { db } from "@/db";
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID!,
+  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
   key_secret: process.env.RAZORPAY_SECRET_ID!,
 });
 
