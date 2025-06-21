c9dc61db53d4b7d0338315113e58dc89121a86cd
samwasted <samarpancodes@gmail.com>
2025-06-21 22:51:10 +0530

    no

--- src/components/ui/UserAccountClient.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import dynamic from 'next/dynamic'
-
-// Use ssr: false here, since this file is a Client Component
-const UserAccountNav = dynamic(() => import('./UserAccountNav'), { ssr: false })
-
-export default UserAccountNav
